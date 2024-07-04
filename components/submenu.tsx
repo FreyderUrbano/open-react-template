8792c38b05f38fbd67430f8d4b82a76a55b3544a
Freyder ¨Inked¨ Urbano <freurbano@hotmail.com>
2024-07-04 18:12:53 -0500

    COMMIT CAMBIOS PAGINA WEB

--- components/submenu.tsx
+++ components/submenu.tsx
@@ -15,7 +15,6 @@ export default function SubMenu() {
                 <div className="py-12 md:py-20">
                     {/* Items */}
                     <div id="borde" className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
-
                         {/* 1st item */}
                         <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
                             <Link href="/canchas" className="relative flex flex-col items-center">
@@ -23,7 +22,6 @@ export default function SubMenu() {
                                 <h4 className="h4 mb-2">Busca tu cancha</h4>
                                 <p className="text-lg text-gray-400 text-center">Elige entre la gran variedad de canchas por sus servicios, por su precio o por su ubicación.</p>
                             </Link>
-
                         </div>
 
                         {/* 2nd item */}
@@ -33,9 +31,7 @@ export default function SubMenu() {
                                 <h4 className="h4 mb-2">Escoge fecha y hora</h4>
                                 <p className="text-lg text-gray-400 text-center">Elige cuando y a que hora deseas jugar tu partido y disfrutar de una mañana, tarde o noche de fútbol y amigos.</p>
                             </Link>
-
                         </div>
-
                         {/* 3rd item */}
                         <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-blocks]">
                             <Link href="/signup" className='relative flex flex-col items-center'>
@@ -43,7 +39,6 @@ export default function SubMenu() {
                                 <h4 className="h4 mb-2">Disfruta</h4>
                                 <p className="text-lg text-gray-400 text-center">Simplemente juega, gana y luego vuelve a reservar.</p>
                             </Link>
-
                         </div>
                     </div>
                 </div>
